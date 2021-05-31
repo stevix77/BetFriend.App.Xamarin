829fc226fbe22caa4f3707906447e9ab4d6822f7
stevix77 <s.valentin77@hotmail.fr>
2021-05-31 23:25:02 +0200

    feat(launchBet) : refactoring

---
+++ BetFriend.Domain/Bets/IBetRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BetFriend.Domain.Bets
+{
+    using BetFriend.Domain.Bets.LaunchBet;
+    using System.Threading.Tasks;
+
+
+    public interface IBetRepository
+    {
+        Task LaunchBetAsync(LaunchBetCommand command);
+    }
+}
