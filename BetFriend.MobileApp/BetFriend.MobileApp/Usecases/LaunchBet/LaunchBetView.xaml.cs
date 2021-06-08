bd76652acc3c8df56ee8383273b3390dffcc7711
steve valentin <s.valentin77@hotmail.fr>
2021-06-08 09:36:31 +0200

    feat(launchBet) : WIP

---
+++ BetFriend.MobileApp/BetFriend.MobileApp/Usecases/LaunchBet/LaunchBetView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BetFriend.MobileApp.Usecases.LaunchBet
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LaunchBetView : ContentPage
+    {
+        public LaunchBetView()
+        {
+            InitializeComponent();
+            //BindingContext = AppShell.
+        }
+    }
+}
