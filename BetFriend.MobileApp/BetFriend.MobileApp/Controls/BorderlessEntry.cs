9e413e078d225c2f8c30b806b9081ef214146301
stevix77 <s.valentin77@hotmail.fr>
2021-06-10 00:46:32 +0200

    feat(launchBet) : launchBet view + call handler

---
+++ BetFriend.MobileApp/BetFriend.MobileApp/Controls/BorderlessEntry.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace BetFriend.MobileApp.Controls
+{
+    /// <summary>
+    /// This class is inherited from Xamarin.Forms.Entry to remove the border for Entry control in the Android platform.
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    public class BorderlessEntry : Entry
+    {
+        public static readonly BindableProperty BorderColorProperty =
+      BindableProperty.Create(nameof(BorderColor),
+          typeof(Color), typeof(BorderlessEntry), Color.Gray);
+        // Gets or sets BorderColor value  
+        public Color BorderColor
+        {
+            get => (Color)GetValue(BorderColorProperty);
+            set => SetValue(BorderColorProperty, value);
+        }
+
+
+        public static readonly BindableProperty IsCurvedCornersEnabledProperty =
+        BindableProperty.Create(nameof(IsCurvedCornersEnabled),
+            typeof(bool), typeof(BorderlessEntry), true);
+        // Gets or sets IsCurvedCornersEnabled value  
+        public bool IsCurvedCornersEnabled
+        {
+            get => (bool)GetValue(IsCurvedCornersEnabledProperty);
+            set => SetValue(IsCurvedCornersEnabledProperty, value);
+        }
+    }
+}
