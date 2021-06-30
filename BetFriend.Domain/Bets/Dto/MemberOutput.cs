04a7679d0a82825586ecb7b831d17a21981dc30b
steve valentin <s.valentin77@hotmail.fr>
2021-06-30 09:21:53 +0200

    DONE handler GetBetsInPRogressOrOverQueryHandler

---
+++ BetFriend.Domain/Bets/Dto/MemberOutput.cs
@@ -0,0 +1,9 @@
+﻿namespace BetFriend.Domain.Bets.Dto
+{
+    using System;
+    public class MemberOutput
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+    }
+}
---
+++ BetFriend.Domain/Bets/GetBetsInProgressOrOver/GetBetsInProgressOrOverQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace BetFriend.Domain.Bets.GetBetsInProgressOrOver
+{
+    using System;
+
+    public sealed class GetBetsInProgressOrOverQuery
+    {
+        public Guid MemberId { get; }
+
+        public GetBetsInProgressOrOverQuery(Guid memberId)
+        {
+            MemberId = memberId;
+        }
+    }
+}
