f086637a23bb63508f6ad2a8a0f2aa0ff7cc2476
steve valentin <s.valentin77@hotmail.fr>
2021-10-30 01:15:36 +0200

    registration

---
+++ BetFriend.Infrastructure/AuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿namespace BetFriend.Infrastructure
+{
+    using BetFriend.Domain.Users;
+
+    public class AuthenticationService : IAuthenticationService
+    {
+        public string Token { get; private set; }
+
+        public void SetToken(string token)
+        {
+            Token = token;
+        }
+    }
+}
---
+++ BetFriend.Infrastructure/HttpService.cs
@@ -0,0 +1,40 @@
+﻿using BetFriend.Infrastructure.Abstractions;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace BetFriend.Infrastructure
+{
+    public class HttpService : IHttpService
+    {
+        private readonly IRestClient _client;
+
+        public HttpService(IRestClient restClient)
+        {
+            _client = restClient;
+        }
+
+        public async Task<T> PostAsync<T>(string url, string json, string token)
+        {
+            try
+            {
+                var req = new RestRequest(url, Method.POST, DataFormat.Json);
+                req.AddHeader("Content-Type", "application/json");
+                req.AddHeader("Language", System.Globalization.CultureInfo.CurrentUICulture.Name);
+                if (!string.IsNullOrEmpty(token))
+                    req.AddHeader("Authorization", token);
+                req.AddJsonBody(json);
+                var response = await _client.ExecuteAsync(req).ConfigureAwait(false);
+                if (!response.IsSuccessful)
+                    throw new Exception(response.Content);
+                return JsonConvert.DeserializeObject<T>(response.Content);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
