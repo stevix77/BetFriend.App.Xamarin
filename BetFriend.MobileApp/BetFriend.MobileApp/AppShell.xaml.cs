d6538b85665d0f7e29755b7f42a75a5747a3ad2b
steve valentin <s.valentin77@hotmail.fr>
2021-11-03 14:46:49 +0100

    save data and logout

--- BetFriend.MobileApp/BetFriend.MobileApp/AppShell.xaml.cs
+++ BetFriend.MobileApp/BetFriend.MobileApp/AppShell.xaml.cs
@@ -1,9 +1,12 @@
 ﻿namespace BetFriend.MobileApp
 {
+    using BetFriend.Domain.Abstractions;
+    using BetFriend.Domain.Users;
     using BetFriend.MobileApp.Views.DetailBet;
     using BetFriend.MobileApp.Views.Home;
     using BetFriend.MobileApp.Views.LaunchBet;
     using BetFriend.MobileApp.Views.Register;
+    using BetFriend.MobileApp.Views.SignIn;
     using Xamarin.Forms;
 
     public partial class AppShell : Shell
@@ -15,6 +18,13 @@
             Routing.RegisterRoute(nameof(HomeView), typeof(HomeView));
             Routing.RegisterRoute(nameof(DetailBetView), typeof(DetailBetView));
             Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
+        }
+
+        private void MenuItem_Clicked(object sender, System.EventArgs e)
+        {
+            ViewModelLocator.Resolve<IAuthenticationService>().SetToken(null);
+            ViewModelLocator.Resolve<IDataStorage>().DeleteData("token");
+            App.Current.MainPage = new SignInPage();
         }
     }
 }
