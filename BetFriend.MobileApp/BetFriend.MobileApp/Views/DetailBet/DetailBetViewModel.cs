21f93a4580ef3078ca9c8aad57c15a3d931bf9ac
steve valentin <s.valentin77@hotmail.fr>
2022-02-28 00:43:18 +0100

    add answer bet in detailBetViewModel

--- BetFriend.MobileApp/BetFriend.MobileApp/Views/DetailBet/DetailBetViewModel.cs
+++ BetFriend.MobileApp/BetFriend.MobileApp/Views/DetailBet/DetailBetViewModel.cs
@@ -1,15 +1,31 @@
 ﻿using BetFriend.Domain.Bets.Dto;
 using BetFriend.Domain.Bets.RetrieveBet;
+using BetFriend.Domain.Bets.Usecases.AnswerBet;
+using BetFriend.Domain.Users;
 using GalaSoft.MvvmLight;
 using System;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace BetFriend.MobileApp.Views.DetailBet
 {
     public class DetailBetViewModel : ViewModelBase
     {
         private BetOutput _bet;
-        private readonly IRetrieveBetQueryHandler _handler;
+        private readonly IRetrieveBetQueryHandler _retrieveBetQueryHandler;
+        private readonly IAnswerBetCommandHandler _answerBetCommandHandler;
+        private readonly IAuthenticationService _authenticationService;
+        private Command _joinBetCommand;
+
+
+        public DetailBetViewModel(IRetrieveBetQueryHandler retrieveBetQueryHandler,
+                                  IAnswerBetCommandHandler answerBetCommandHandler,
+                                  IAuthenticationService authenticationService)
+        {
+            _retrieveBetQueryHandler = retrieveBetQueryHandler;
+            _answerBetCommandHandler = answerBetCommandHandler;
+            _authenticationService = authenticationService;
+        }
 
         public BetOutput Bet
         {
@@ -21,14 +37,20 @@ namespace BetFriend.MobileApp.Views.DetailBet
             }
         }
 
-        public DetailBetViewModel(IRetrieveBetQueryHandler retrieveBetQueryHandler)
+        public Command JoinBetCommand
         {
-            _handler = retrieveBetQueryHandler;
+            get => _joinBetCommand ?? (_joinBetCommand = new Command(async () =>
+            {
+                var command = new AnswerBetCommand(_bet.Id.ToString(), true);
+                await _answerBetCommandHandler.Handle(command);
+            }, () => _bet.Creator.Id != Guid.Parse(_authenticationService.UserId)));
         }
 
         internal async Task LoadBet(string value)
         {
-            Bet = await _handler.Handle(new RetrieveBetQuery(Guid.Parse(value)));
+            Bet = await _retrieveBetQueryHandler.Handle(new RetrieveBetQuery(Guid.Parse(value)));
         }
+
+
     }
 }
